c31517eaa62e24247582bb4809ddf2ed0f671829
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-11-11 14:22:34 -0300

    inicio basado en clase

--- src/comercio/views.py
+++ src/comercio/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts          import render
+from django.views.generic.base import TemplateView
 
 from apps.productos.models import Producto
 
+# Vista basa en funcion
+"""
 def inicio(request):
 	template_name = "index.html"
 	ultimos_productos = Producto.objects.all().order_by('-id')[:2]
@@ -10,14 +13,13 @@ def inicio(request):
 		'ultimos_productos': ultimos_productos
 	}
 	return render(request, template_name, ctx)
-
 """
-def login(request):
-	template_name = "login.html"
 
-	ctx = {
+# Vista basa en clases
+class Inicio(TemplateView):
+	template_name = "index.html"
 
-	}
-	return render(request, template_name, ctx)
-
-"""
+	def get_context_data(self, **kwargs):
+		context = super(Inicio, self).get_context_data(**kwargs)
+		context["ultimos_productos"] = Producto.objects.all().order_by('-id')[:2]
+		return context
