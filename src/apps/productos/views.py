9c46c383c68698cabeef9d1c5dfd24c413bc67ff
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-11-16 13:55:21 -0300

    crear producto

--- src/apps/productos/views.py
+++ src/apps/productos/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts           import render
-from django.views.generic.list  import ListView
+from django.contrib.auth.mixins     import LoginRequiredMixin
+from django.shortcuts               import render
+from django.urls                    import reverse_lazy
+from django.views.generic           import ListView, CreateView
 
+from .forms  import ProductoForm
 from .models import Producto
 """
 @login_required
@@ -27,3 +29,24 @@ class Listar(LoginRequiredMixin, ListView):
 
 	def get_queryset(self):
 		return Producto.objects.filter(activo=True).order_by("nombre")
+
+
+# ======================================================
+# 					Vistas para Admin
+# ======================================================
+class ListarAdmin(LoginRequiredMixin, ListView):
+	template_name = "productos/admin/listar.html"
+	model = Producto
+	context_object_name = 'lista_productos'
+	paginate_by = 10
+
+	def get_queryset(self):
+		return Producto.objects.all().order_by("id")
+
+class Crear(LoginRequiredMixin, CreateView):
+	template_name = "productos/admin/nuevo.html"
+	model = Producto
+	form_class = ProductoForm
+
+	def get_success_url(self, **kwargs):
+		return reverse_lazy("productos:admin_listar")
