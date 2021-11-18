d9321d9a79e72c58deb9871722a9a5019788f386
Lucas Ibañez <lucas.ibanez@gfe.frre.utn.edu.ar>
2021-11-18 14:18:28 -0300

    foreignkey

--- src/apps/productos/admin.py
+++ src/apps/productos/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Producto
+from .models import Producto, Categoria
 
 class ProductoAdmin(admin.ModelAdmin):
-	list_display = ["id","nombre", "precio", "cantidad", "activo"]
+	list_display = ["id","nombre", "precio", "cantidad", "activo", "categoria"]
 
 admin.site.register(Producto, ProductoAdmin)
+
+class CategoriaAdmin(admin.ModelAdmin):
+	list_display = ["id","nombre"]
+
+admin.site.register(Categoria, CategoriaAdmin)
--- src/apps/productos/models.py
+++ src/apps/productos/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Categoria(models.Model):
+	nombre = models.CharField(max_length=250)
+
+	class Meta:
+		db_table="categorias"
+
+	def __str__(self):
+		return self.nombre
 
 class Producto(models.Model):
 	nombre = models.CharField(max_length=250)
@@ -10,8 +18,13 @@ class Producto(models.Model):
 	activo = models.BooleanField(default=True)
 	imagen = models.ImageField(upload_to="productos", null=True, blank=True)
 
+	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="productos_relacionados", null=True)
+
 	class Meta:
 		db_table="productos"
 
 	def __str__(self):
 		return f"[{self.id}] {self.nombre}"
+
+
+
